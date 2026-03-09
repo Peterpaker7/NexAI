34d3b594830c49826a7f8bee0a63dfd277a12be2
Mukesh.K.A <168621970+Peterpaker7@users.noreply.github.com>
2026-03-09 07:40:19 +0530

    nexai-cp

---
+++ git-nexai-cp/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ClinicianDashboard from './ClinicianDashboard';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ClinicianDashboard />
+  </React.StrictMode>
+);
